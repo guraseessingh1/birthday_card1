32831b9c1d9dec26cbf36f06025a02813f5d2d76
guraseessingh1 <gssmasuta@gmail.com>
2024-09-04 19:57:22 +0100

    Add files via upload

---
+++ birthday_card_animation/main.py
@@ -0,0 +1,9 @@
+import pygame
+import time
+
+pygame.init()
+WIDTH = 600
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("birthday greeting")
+image = pygame.image.load("images/baloons.jpeg")
